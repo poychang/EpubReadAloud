63102d7d9b9c2a3069b7ab583aae07bdf3bdb43c
Poy Chang <poypost@gmail.com>
2021-11-06 02:29:31 +0800

    init project

---
+++ EpubReadAloud/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Tewr.Blazor.FileReader;
+
+namespace EpubReadAloud
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
